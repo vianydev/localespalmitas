3537d975a61fa46c05bfeefafaf0f76d16a9437c
vianey <vianymarcelo@gmail.com>
2025-03-20 11:13:40 -0600

    add files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: EstateAgency
-* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
-* Updated: Aug 09 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
